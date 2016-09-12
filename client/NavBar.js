c631172540cfb94e4fdbe45283cf0b73298d5600
Peter Chim <pchim24@gmail.com>
2016-09-11 23:21:51 -0700

    Update styling

--- client/NavBar.js
+++ client/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => (
       </div>
       <div id="navbar" className="collapse navbar-collapse">
         <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
           <li><a href="#about">About</a></li>
         </ul>
       </div>
